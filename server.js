5d2b31d3606df28a74b4fa8b6b091c875175e6d4
Evgene Sotnikov <esotnikov037@gmail.com>
2019-11-04 01:03:52 +0200

    fix server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const app = require();
-app.use(express.static(__dirname + 'dist/Test2'));
-app.get('/', function(req, res) {
+const app = express();
+
+app.use(express.static(__dirname + '/dist/Test2'));
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/Test2/index.html'));
 });
 app.listen(process.env.PORT || 8080);
